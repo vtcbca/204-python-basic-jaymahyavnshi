c20b9e57b981957004f179a0079af0fa04bbb31d
jaymahyavnshi <101244553+jaymahyavnshi@users.noreply.github.com>
2022-03-10 13:47:46 +0530

    solution
    here i am uploading my journal programs

---
+++ solution/solution 1.py
@@ -0,0 +1,7 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Write a python script to enter two No. & print the sum of it.
+a=int(input("Enter value for a : "))
+b=int(input("Enter value for b : "))
+c=a+b
+print("Sum of {} and {} = {}".format(a,b,c))
